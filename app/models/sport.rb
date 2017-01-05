b290694c1c1b4a5cfb5bebe52fd80f88d07be5f4
Megan Eding & Martin Murray & Christian Salas & Sarav Shah <pair+meganeding1.mjmurra4.salasc2.saravshah@devbootcamp.com>
2017-01-04 18:29:16 -0800

    Fix sport and games associations

--- app/models/sport.rb
+++ app/models/sport.rb
@@ -1,5 +1,5 @@
 class Sport < ApplicationRecord
   has_many :teams
-  has_many :game_teams, through: :teams, source: :game_teams
-  has_many :games, through: :game_teams, source: :games
+  has_many :game_teams, through: :teams, class_name: "GameTeam"
+  has_many :games, through: :game_teams, class_name: "Game"
 end
