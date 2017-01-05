38deb86cf116ffb1282e54e151472f4b543a5fe8
SalasC2 <christiansalas96@gmail.com>
2017-01-04 19:06:44 -0800

    Add user views and the controllers that go along with it

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/' => 'home#index'
+
+  get '/users/:id/profile' => 'users#profile'
+  get '/users/new' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
 end
