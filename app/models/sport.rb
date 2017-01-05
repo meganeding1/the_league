3c1bd103eb094b66df13aea73607137f24c91497
Megan Eding & Martin Murray & Christian Salas & Sarav Shah <pair+meganeding1.mjmurra4.salasc2.saravshah@devbootcamp.com>
2017-01-05 11:56:33 -0800

    Sport page WIP, change validations for sport model

--- app/models/sport.rb
+++ app/models/sport.rb
@@ -2,4 +2,6 @@ class Sport < ApplicationRecord
   has_many :teams
   has_many :game_teams, through: :teams, class_name: "GameTeam"
   has_many :games, through: :game_teams, class_name: "Game"
+
+  validates :name, presence: true, uniqueness: true
 end
--- db/migrate/20170105000105_create_sports.rb
+++ db/migrate/20170105000105_create_sports.rb
@@ -1,7 +1,7 @@
 class CreateSports < ActiveRecord::Migration[5.0]
   def change
     create_table :sports do |t|
-      t.string :name, null: false
+      t.string :name
       t.text :description
 
       t.timestamps
