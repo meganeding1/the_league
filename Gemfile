2f2eb810b9fdc3027919159706c38fd8aa2783df
Megan Eding & Martin Murray & Christian Salas & Sarav Shah <pair+meganeding1.mjmurra4.salasc2.saravshah@devbootcamp.com>
2017-01-05 10:15:04 -0800

    Add seeds to database

--- Gemfile
+++ Gemfile
@@ -21,7 +21,6 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
-
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
@@ -33,6 +32,7 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 gem 'slim'
+gem 'faker'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,84 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+User.destroy_all
+Sport.destroy_all
+Team.destroy_all
+Game.destroy_all
+60.times do
+  User.create({
+    username: Faker::GameOfThrones.character,
+    email: Faker::Internet.email,
+    password: 'password',
+    phone_number: Faker::PhoneNumber.phone_number
+    })
+end
+
+5.times do
+  Sport.create({
+    name: Faker::Team.sport,
+    description: Faker::Lorem.paragraph
+    })
+end
+
+10.times do
+  Team.create({
+    name: Faker::Team.name,
+    user_id: User.all.sample.id,
+    sport_id: Sport.all.sample.id
+    })
+end
+
+20.times do
+  Game.create({
+    date: Faker::Date.between_except(1.year.ago, 1.year.from_now, Date.today),
+    team_1: Team.all.sample.name,
+    team_2: Team.all.sample.name
+    })
+end
+
+
+# 5.times do
+#   school_name = Faker::University.name
+#   School.create({
+#     name: school_name,
+#     address: Faker::Address.street_address,
+#     city: Faker::Address.city,
+#     state: Faker::Address.state,
+#     zipcode: Faker::Address.zip,
+#     admin_code: "#{school_name}+a",
+#     teacher_code: "#{school_name}+t"
+#     })
+# end
+# 15.times do
+#   Teacher.create({ name: Faker::Beer.name,
+#                   email: Faker::Internet.email,
+#                password: 'password',
+#                   admin: false,
+#                  school: School.all.sample
+#     })
+# end
+# 30.times do
+#   our_school = School.all.sample
+#   Team.create({  name: Faker::GameOfThrones.character,
+#                school: our_school,
+#               teacher: our_school.teachers.sample
+#     })
+# end
+# gender = %w[female male non-binary fluid]
+# animal = %w[unicorn wolf elephant tiger eagle capybara racoon dragon mouse butterfly]
+# shirts = %w[small, medium, large]
+# 200.times do
+#   our_school = School.all.sample
+#     Student.create({
+#                     school: our_school,
+#                       team: our_school.teams.sample,
+#                       name: Faker::Name.name,
+#                      grade: rand(1..12),
+#                     gender: gender.sample,
+#                        GPA: (rand * (4) + 1).round(1),
+#                 detentions: rand(0..5),
+#                 fav_animal: animal.sample,
+#                 shirt_size: shirts.sample,
+#                  allergies: 'none',
+#          emergency_contact: 'Mom'
+#         })
+# end
