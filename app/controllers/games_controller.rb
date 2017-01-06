0735d387a8c4857b5d1fa90f10332df3ca19e4bb
SalasC2 <christiansalas96@gmail.com>
2017-01-06 12:29:33 -0800

    Add create game to team profile page

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,11 +1,32 @@
 class GamesController < ApplicationController
   def index
-    @sport = Sport.find(params[:sport_id])
-    @games = @sport.games
+    @teams = Team.find(params[:team_id])
+    @games = @team.games
+  end
+
+  def new
+    @team = Team.find(params[:team_id])
+    @game = Game.new
+  end
+
+  def create
+    @team = Team.find(params[:team_id])
+    @game = @team.games.new(game_params)
+
+    if @game.save
+      redirect_to team_games_path
+    else
+      render 'new'
+    end
   end
 
   def show
     @game = Game.find(params[:id])
+  end
 
+  private
+
+  def game_params
+    params.require(:game).permit(:date, :team_1, :team_2)
   end
 end
